b0a28d06d17bec889ea9160efff1092d00dd20e7
mbah37 <mbah37@student.cscc.edu>
2025-10-26 08:42:12 -0400

    added comments to coin.py

--- Lab 9/coin.py
+++ Lab 9/coin.py
@@ -1,3 +1,7 @@
+#Moustapha Bah
+#Coin class used for Coin Matching Game
+#10/25/2025
+
 import random 
 
 class Coin:
@@ -6,16 +10,20 @@ class Coin:
         self.__amount = 20
     
     def toss(self):
+        # Sets the side up of the coin to heads or tails randomly
         self.__sideup = "Heads" if random.randint(0, 1) == 0 else "Tails"
     
     def get_sideup(self):
+        # Returns the current side up of the coin
         return self.__sideup
     
     def get_amount(self):
+       # Returns the current amount of coins the player has
         return self.__amount
     
     def set_amount(self, change):
-        self.__amount -= change
+        # Updates the amount of coins the player has
+        self.__amount += change
 
     
     
