455a1d680cb840261b0607fbafcc57b205a50ad2
mbah37 <mbah37@student.cscc.edu>
2025-10-30 10:58:05 -0400

    refactored and added comments

--- Lab 10/Lab_10_mbah37-1.py
+++ Lab 10/Lab_10_mbah37-1.py
@@ -4,44 +4,61 @@
 
 from pathlib import Path
 
+# Function to count the frequency of each word in the file
 def count_word_frequency(fptr):
     word_count = {}
+    '''Reads the first line from the file'''
     line = fptr.readline()
+    
+    ''' List of punctuation characters to be removed from the text'''
     punctChars = ('.', ',', '!', '?', ';', ':', '"', "'", '(', ')', '[', ']', '{', '}', '-', '_', '\n', '\r')
-
+    
+    ''' Process each line until the end of the file'''
     while line:
         for c in punctChars:
+            ''' Replace each punctuation character with a space'''
             line = line.replace(c, ' ')
-    
+        ''' Splitting the line into words'''
         words = line.split()
+        
+        ''' Counting the frequency of each word'''
         for word in words:
             tmp = word.lower()
             word_count[tmp] = word_count.get(tmp, 0) + 1
+        ''' Read the next line from the file until the end of the file'''
         line = fptr.readline()
+    
     return word_count
 
 def print_word_frequency(word_count):
+    ''' Print the word frequency in alphabetical order'''
     for word, count in sorted(word_count.keys()):
         print(f"{word}: {count}")
 
-
+'''' Main function to handle user input and file processing'''
 def main():
+    ''' Loop until a valid file is provided'''
     while True:
         file_name = input("Enter the filename: ")
    
         try:
+            ''' Open the file for reading'''
             fptr = open(file_name,'r')
  
+            '''Prints if file is not found '''
         except FileNotFoundError:
             print(f" Error: The file '{file_name}' was not found. Try again.")
-        
+
+            ''' Prints if file format is not correct/cannot be read '''
         except ValueError:
-            print(f" Error: Could not read the file '{file_name}'.Please check the file format.")
+            print(f" Error: Could not read the file '{file_name}'.Please check the file format(.txt).")
         
+            ''' If file is opened successfully, process the file '''
         else:
             word_count = count_word_frequency(fptr)
             print_word_frequency(word_count)
             fptr.close()
+           
             break
 
 
