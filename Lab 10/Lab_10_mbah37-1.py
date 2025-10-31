b7d31a13287478b7c4a1f729df8afad1e311d73a
mbah37 <mbah37@student.cscc.edu>
2025-10-30 21:01:01 -0400

    refactored more and added more comments. created seperate variable for contractions (forgot to mention in previous commit)

--- Lab 10/Lab_10_mbah37-1.py
+++ Lab 10/Lab_10_mbah37-1.py
@@ -6,43 +6,55 @@ from pathlib import Path
 
 # Function to count the frequency of each word in the file
 def count_word_frequency(fptr):
+    
     word_count = {}
+    
     '''Reads the first line from the file'''
     line = fptr.readline()
     
     ''' List of punctuation characters to be removed from the text'''
-    punctChars = (".", ",", "!", "?", ";", ":", "(", ")", "[", "]", "{", "}", "-", "_", "\n", "\r")
+    punctChars = ('.', ',', '!', '?', ';', ':', '(', ')', '[', ']', '{', '}', '-', '_', '\n', '\r')
     
     ''' Process each line until the end of the file'''
     while line:
+        
         ''' Replace each punctuation character with a space'''
         for c in punctChars:
             line = line.replace(c, ' ')
         
-        contractions = []
         words = line.split()
+        contractions = []
+
         
         ''' Counting the frequency of each word'''
         for word in words:
+           
+            ''' if the word contains an apostrophe, it is considered a contraction'''
             word = word.strip('\'"')  # Removes leading/trailing quotes
             word = word.lower() 
+            
+            ''' Add the word to the contractions list if it is not empty'''
             if word:
                 contractions.append(word)
-
+        
             for tmp in contractions:   
                 word_count[tmp] = word_count.get(tmp, 0) + 1
+       
         ''' Read the next line from the file until the end of the file'''
         line = fptr.readline()
     
     return word_count
 
 def print_word_frequency(word_count):
+    
     ''' Print the word frequency in alphabetical order'''
     for word, count in sorted(word_count.items()):
         print(f"{word}: {count}")
 
+
 '''' Main function to handle user input and file processing'''
 def main():
+    
     ''' Loop until a valid file is provided'''
     while True:
         file_name = input("Enter the filename: ")
@@ -61,7 +73,6 @@ def main():
             print(f" Error: Could not read the file '{file_name}'.Please check the file format(.txt).")
         
             ''' If file is opened successfully, process the file '''
-        
         else:
             word_count = count_word_frequency(fptr)
             print_word_frequency(word_count)
