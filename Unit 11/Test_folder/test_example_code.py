8fa9937e775dac0df38797ba361ad88affd77151
mbah37 <mbah37@student.cscc.edu>
2025-11-04 09:51:20 -0500

    tried new unit tests

--- Unit 11/Test_folder/test_example_code.py
+++ Unit 11/Test_folder/test_example_code.py
@@ -3,5 +3,7 @@ from example_code import divide
 
 
 def test_divide_with_correct_input():
+    # positive test cases
     assert divide(10, 2) == 5
-    
+    assert divide(6, 3) == 2
+    assert divide(100, 25) == 4
