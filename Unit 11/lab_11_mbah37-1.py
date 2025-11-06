29782027ab6c8343524ac5c5e0fe4089d8d606a6
mbah37 <mbah37@student.cscc.edu>
2025-11-06 11:46:59 -0500

    created normal_angle function

---
+++ Unit 11/lab_11_mbah37-1.py
@@ -0,0 +1,19 @@
+# Moustapha Bah
+# 11/06/2025
+# function for inputting a rotating angle(degrees) and normalize it by removing 
+# unnecessary full 360° rotations 
+
+def normal_angle():
+    while True: 
+        try:
+            degrees = float(input("Enter the rotating angle in degrees: "))
+        except (ValueError, TypeError):
+            print("Invalid input: Please enter a numeric value for degrees.")
+            continue
+        else:
+            rotating_angle = degrees % 360
+            return rotating_angle
+
+print(normal_angle())  # Example usage
+
+
