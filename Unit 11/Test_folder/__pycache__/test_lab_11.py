d73d2e260b3ab48360ee7040fb36bb83a13abd5a
mbah37 <mbah37@student.cscc.edu>
2025-11-06 20:17:19 -0500

    renamed test file

---
+++ Unit 11/Test_folder/__pycache__/test_lab_11.py
@@ -0,0 +1,10 @@
+import pytest
+from lab_11_mbah37_1 import normal_angle
+
+def test_normal_angle_valid_input():
+    assert normal_angle(100) == 100
+    assert normal_angle(460) == 100
+    assert normal_angle(820) == 100
+    assert normal_angle(-100) == 260
+    assert normal_angle(-460) == 260
+    assert normal_angle(-820) == 260
