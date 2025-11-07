ba2035ab145f5b4b9081ac5612e5e8175e7c5bbb
mbah37 <mbah37@student.cscc.edu>
2025-11-06 20:26:32 -0500

    accessing pytest

--- Unit 11/Test_folder/test_lab_11.py
+++ Unit 11/Test_folder/test_lab_11.py
@@ -8,3 +8,6 @@ def test_normal_angle_valid_input():
     assert normal_angle(-100) == 260
     assert normal_angle(-460) == 260
     assert normal_angle(-820) == 260
+
+with pytest.raises((ValueError, TypeError)):
+    normal_angle("abc")
