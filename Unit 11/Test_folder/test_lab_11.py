d7d987f1ec6853b893515c6df699bb9badeb0300
mbah37 <mbah37@student.cscc.edu>
2025-11-08 09:00:47 -0500

    tried to add invalid input tests but did not test

--- Unit 11/Test_folder/test_lab_11.py
+++ Unit 11/Test_folder/test_lab_11.py
@@ -9,5 +9,12 @@ def test_normal_angle_valid_input():
     assert normal_angle(-460) == 260
     assert normal_angle(-820) == 260
 
-with pytest.raises((ValueError, TypeError)):
-    normal_angle("abc")
+def test_normal_angle_invalid_input():
+    with pytest.raises(ValueError):
+        normal_angle("abc")
+    with pytest.raises(ValueError):
+        normal_angle(None)
+    with pytest.raises(ValueError):
+        normal_angle([])
+    with pytest.raises(ValueError):
+        normal_angle({})
