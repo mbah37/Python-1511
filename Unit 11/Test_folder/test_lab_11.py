e964a3c0a0380e21a32090c16abcf3e68949cdf7
mbah37 <mbah37@student.cscc.edu>
2025-11-08 09:06:23 -0500

    added comments for both test cases

--- Unit 11/Test_folder/test_lab_11.py
+++ Unit 11/Test_folder/test_lab_11.py
@@ -2,6 +2,7 @@ import pytest
 from lab_11_mbah37_1 import normal_angle
 
 def test_normal_angle_valid_input():
+    # Test cases for valid inputs
     assert normal_angle(100) == 100
     assert normal_angle(460) == 100
     assert normal_angle(820) == 100
@@ -10,6 +11,7 @@ def test_normal_angle_valid_input():
     assert normal_angle(-820) == 260
 
 def test_normal_angle_invalid_input():
+    # Test cases for invalid inputs returning None
     assert normal_angle("abc") == None
     assert normal_angle(None) == None
     assert normal_angle([]) == None
