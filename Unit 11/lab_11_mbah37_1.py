5b1407ea052793e1f8eae9f65300cf43d3799822
mbah37 <mbah37@student.cscc.edu>
2025-11-08 09:02:53 -0500

    removed while loop.

--- Unit 11/lab_11_mbah37_1.py
+++ Unit 11/lab_11_mbah37_1.py
@@ -4,15 +4,14 @@
 # unnecessary full 360° rotations 
 
 def normal_angle(degrees):
-    while True: 
-        try:
-            degrees = float(degrees)
-        except (ValueError, TypeError):
-            print("Invalid input: Please enter a numeric value for degrees.")
-            continue
-        else:
-            rotating_angle = degrees % 360
-            return rotating_angle
+    try:
+        degrees = float(degrees)
+    except (ValueError, TypeError):
+        print("Invalid input: Please enter a numeric value for degrees.")
+        return None    
+    else:
+        rotating_angle = degrees % 360
+        return rotating_angle
 
 
 
