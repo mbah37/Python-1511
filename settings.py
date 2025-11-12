08de9dddaa6ca057d3eb3f5bbe49c3b475993e06
mbah37 <mbah37@student.cscc.edu>
2025-11-12 11:43:52 -0500

    added a new backgeound and changes the settings class to fit the image to scale

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ class Settings:
         self.screen_width : int = 1200
         self.screen_height : int = 800
         self.FPS : int = 60
-        self.bg_file : Path = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
+        self.bg_file : Path = Path.cwd() / 'Assets' / 'images' / 'Winterfell_background.png'
 
 
