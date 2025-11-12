b632df5e663ab467176557501f3f13e23bb27a8e
mbah37 <mbah37@student.cscc.edu>
2025-11-12 10:44:52 -0500

    2nd tutorial. created a settings file/class and changed the background to be an image scaled to the screen. (planning on changed background soon)

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,24 @@
 import sys
 import pygame
-
+from settings import Settings
 
 class AlienInvasion:
 
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        
+        pygame.display.set_caption(self.settings.name)
+
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg,
+             (self.settings.screen_width, self.settings.screen_height))
 
         self.running = True
+        self.clock = pygame.time.Clock()
     
     def run_game(self):
         while self.running:
@@ -19,8 +27,9 @@ class AlienInvasion:
                     self.running = False
                     pygame.quit()
                     sys.exit()
-            
+            self.screen.blit(self.bg, (0, 0))
             pygame.display.flip()
+            self.clock.tick(self.settings.FPS)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
---
+++ settings.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+class Settings:
+
+    def __init__(self):
+        self.name : str = 'White Walker Invasion'
+        self.screen_width : int = 1200
+        self.screen_height : int = 800
+        self.FPS : int = 60
+        self.bg_file : Path = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
+
+
