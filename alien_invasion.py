8f728d64fc7390bfd658a4739e568e97725c4b60
mbah37 <mbah37@student.cscc.edu>
2025-11-12 15:11:01 -0500

    change alien invasion class to whitewalker invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -82,37 +82,3 @@ if __name__ == '__main__':
     ai.run_game()
 
 
-from settings import Settings
-
-class AlienInvasion:
-
-    def __init__(self):
-        pygame.init()
-        self.settings = Settings()
-
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
-        
-        pygame.display.set_caption(self.settings.name)
-
-        self.bg = pygame.image.load(self.settings.bg_file)
-        self.bg = pygame.transform.scale(self.bg,
-             (self.settings.screen_width, self.settings.screen_height))
-
-        self.running = True
-        self.clock = pygame.time.Clock()
-    
-    def run_game(self):
-        while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                    pygame.quit()
-                    sys.exit()
-            self.screen.blit(self.bg, (0, 0))
-            pygame.display.flip()
-            self.clock.tick(self.settings.FPS)
-
-if __name__ == '__main__':
-    ai = AlienInvasion()
-    ai.run_game()
