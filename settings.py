19c0474f5d292d6271c892594ed0cde9e36e09dd
mbah37 <mbah37@student.cscc.edu>
2025-11-13 23:18:04 -0500

    dragons were created after images. I thought it was bc they weren't transparent but the winterfell bg wasnt fully transparent so i deleted it and created a new one.

--- settings.py
+++ settings.py
@@ -6,9 +6,9 @@ class Settings:
         self.screen_width : int = 1200
         self.screen_height : int = 700
         self.FPS : int = 60
-        self.bg_file : Path = Path.cwd() / 'Assets' / 'images' / 'Winterfell_background.png'
+        self.bg_file : Path = Path.cwd() / 'Assets' / 'images' / 'Winterfell1.png'
 
-        self.dragon_file : Path = Path.cwd() / 'Assets' / 'images' / 'Drogon1.png'
+        self.dragon_file : Path = Path.cwd() / 'Assets' / 'images' / 'Rhaegal2.png'
         self.dragon_width : int = 100
         self.dragon_height : int = 100
         self.dragon_speed : int = 5
